51d0dc296ae6b6c1a5c1b436d98bdeea453f91f5
Chad Duffey <chadduffey@hotmail.com>
2015-04-10 08:25:00 +1000

    Initial Commit

---
+++ Downloading Web Content/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Downloading Web Content
+//
+//  Created by Chad Duffey on 10/04/2015.
+//  Copyright (c) 2015 Chad Duffey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
