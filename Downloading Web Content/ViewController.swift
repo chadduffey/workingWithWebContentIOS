e5093e92a14aa34d999eff6a550cc2bca99d29dd
Chad Duffey <chadduffey@hotmail.com>
2015-04-13 17:19:12 +1000

    web pull

--- Downloading Web Content/ViewController.swift
+++ Downloading Web Content/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var newWebView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.stackoverflow.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!){
+            (data,response,error) in
+            
+            if error == nil {
+                var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+                //added this later to deal with asyncronus nature of the call. This says stop waiting
+                //and do immediately. This reduces delays in HTML handling for us. 
+                dispatch_async(dispatch_get_main_queue()){
+                    
+                    //this was the orginal display to the page, without async handling
+                    self.newWebView.loadHTMLString(urlContent, baseURL: nil)
+                }
+                
+            }
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
